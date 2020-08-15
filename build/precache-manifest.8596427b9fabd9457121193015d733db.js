e0533431c806ea47e54ab6761fe93a5ff07a3721
amir vansover <amir@vansover.com>
2020-08-15 16:20:46 +0300

    wpKeshev

---
+++ build/precache-manifest.8596427b9fabd9457121193015d733db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f32b1d3bba11ef8b0477224d6e51e89",
+    "url": "/wpKeshev/index.html"
+  },
+  {
+    "revision": "98531ef015379b03e230",
+    "url": "/wpKeshev/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a1d9b7a744e16ba5c1c7",
+    "url": "/wpKeshev/static/css/main.bd4c681f.chunk.css"
+  },
+  {
+    "revision": "98531ef015379b03e230",
+    "url": "/wpKeshev/static/js/2.e075a4d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wpKeshev/static/js/2.e075a4d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d9b7a744e16ba5c1c7",
+    "url": "/wpKeshev/static/js/main.bfbe3c80.chunk.js"
+  },
+  {
+    "revision": "a3bdae3769899bc10afd",
+    "url": "/wpKeshev/static/js/runtime-main.9881c503.js"
+  }
+]);
